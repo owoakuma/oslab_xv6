2f30e6a93c0724a424514bb5de47ac7924488351
Alley_Niko <lerversonallen@gmail.com>
2024-03-22 15:16:31 +0800

    cmake_test

---
+++ build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/src/main.c.o"
+  "CMakeFiles/demo.dir/src/main.c.o.d"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("hello cmake");
+    return 0;
+}
